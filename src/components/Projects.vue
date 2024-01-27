cb63fcbf4d6248dbcc0a1cc4b865979c6c28265b
campbell-frost <campbellsfrost@gmail.com>
2024-01-27 16:16:25 -0500

    modals appear sometimes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="d-flex justify-content-start padding-projects mt-5" id="featured-work">
         <div class="container">
-            <div v-for="(project, index) in projects" :key="index" @click="openModal(index)">
-                <div class="row" v-if="index % 2 === 0">
+            <div class="section-title">
+                <h2>Projects</h2>
+            </div>
+            <div v-for="(project, index) in projects" :key="index" @click="openModal(index)" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">
+                <div class="row pb-4" v-if="index % 2 === 0">
                     <div class="col-md-6">
                         <div class="project-box">
                             <div class="project-card">
@@ -32,8 +36,8 @@
         </div>
 
         <!-- Modal -->
-        <div class="modal fade" v-if="selectedProject !== null" tabindex="-1" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
+        <div class="modal" v-if="selectedProject !== null" id="exampleModal" tabindex="-1"
+            aria-labelledby="exampleModalLabel" aria-hidden="true"> aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -76,36 +80,52 @@ let selectedProject = null;
 
 const openModal = (index) => {
     selectedProject = projects[index];
+    console.log(selectedProject)
 };
 
 const closeModal = () => {
-    selectedProject = null;
+    selectedProject = "Modal Closed";
 };
+
 </script>
 
 <style scoped>
 .project-card {
+    position: relative;
     width: 100%;
     height: 100%;
     background-color: #eee;
     padding: 100px;
-    /* Adjust the padding value as needed */
+    border-radius: 8px;
     cursor: pointer;
     transition: background-color 0.3s;
 }
 
 .project-card:hover {
     background-color: #959494;
+    border-radius: 8px;
 
 }
 
 .project-info {
-    display: none;
-    /* Initially hide text */
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    width: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, 0.7);
     color: white;
+    transition: opacity 0.3s;
+    opacity: 0;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+
 }
 
 .project-card:hover .project-info {
-    display: block;
+    opacity: 1;
     /* Show text on hover */
-}</style>
+}
+</style>
