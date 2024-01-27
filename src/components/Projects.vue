7a440fefd5a60d3ad00c2f42c7b72dd6312107b7
campbell-frost <campbellsfrost@gmail.com>
2024-01-27 16:40:34 -0500

    fixed modals

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,52 +1,32 @@
 <template>
-    <div class="d-flex justify-content-start padding-projects mt-5" id="featured-work">
-        <div class="container">
-            <div class="section-title">
-                <h2>Projects</h2>
-            </div>
-            <div v-for="(project, index) in projects" :key="index" @click="openModal(index)" data-bs-toggle="modal"
-                data-bs-target="#exampleModal">
-                <div class="row pb-4" v-if="index % 2 === 0">
-                    <div class="col-md-6">
-                        <div class="project-box">
-                            <div class="project-card">
-                                <img :src="project.image" alt="Project Image" class="img-fluid">
-                                <div class="project-info">
-                                    <h4>{{ project.name }}</h4>
-                                    <p>{{ project.company }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Check if there is another project for the second column -->
-                    <div v-if="index + 1 < projects.length" class="col-md-6">
-                        <div class="project-box">
-                            <div class="project-card">
-                                <img :src="projects[index + 1].image" alt="Project Image" class="img-fluid">
-                                <div class="project-info">
-                                    <h4>{{ projects[index + 1].name }}</h4>
-                                    <p>{{ projects[index + 1].company }}</p>
-                                </div>
-                            </div>
-                        </div>
+    <div>
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-6" v-for="project in projects" :key="project.id">
+                    <div class="project-box" @click="openModal(project)">
+                        <img :src="project.image" class="img-fluid" alt="Project Image">
+                        <h3>{{ project.title }}</h3>
+                        <p>{{ project.company }}</p>
                     </div>
                 </div>
             </div>
         </div>
 
         <!-- Modal -->
-        <div class="modal" v-if="selectedProject !== null" id="exampleModal" tabindex="-1"
-            aria-labelledby="exampleModalLabel" aria-hidden="true"> aria-hidden="true">
+        <div class="modal" ref="projectModal" tabindex="-1" aria-labelledby="projectModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">{{ selectedProject.name }}</h5>
-                        <button type="button" class="btn-close" @click="closeModal"></button>
+                        <h5 class="modal-title" id="projectModalLabel" v-if="selectedProject">{{ selectedProject.title }}
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body" v-if="selectedProject !== null">
-                        <img :src="selectedProject.image" alt="Project Image" class="img-fluid">
+                    <div class="modal-body" v-if="selectedProject">
+                        <img :src="selectedProject.image" class="img-fluid" alt="Project Image">
                         <p>{{ selectedProject.description }}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </div>
                 </div>
             </div>
@@ -54,78 +34,59 @@
     </div>
 </template>
 
-<script setup>
-const projects = [
-    {
-        name: 'Sign Me In',
-        company: 'FDTC',
-        image: './src/components/images/projects/sign-me-in.png',
-        description: 'Description of Project 1.',
+<script>
+export default {
+    data() {
+        return {
+            projects: [
+                {
+                    id: 1,
+                    title: 'Project 1',
+                    company: 'Company A',
+                    image: './src/components/images/projects/sign-me-in.png',
+                    description: 'Description for Project 1...',
+                },
+                {
+                    id: 2,
+                    title: 'Project 2',
+                    company: 'Company B',
+                    image: './src/components/images/projects/mcleodHealth.jpg',
+                    description: 'Description for Project 2...',
+                },
+                {
+                    id: 3,
+                    title: 'Project 3',
+                    company: 'Company C',
+                    image: './src/components/images/projects/bossybooks.png',
+                    description: 'Description for Project 3...',
+                },
+            ],
+            selectedProject: null,
+        };
     },
-    {
-        name: 'Morgue Tracker',
-        company: 'McLeod Health',
-        image: './src/components/images/projects/mcleodHealth.jpg',
-        description: 'Description of Project 2.',
+    methods: {
+        openModal(project) {
+            this.selectedProject = project;
+            const modal = new bootstrap.Modal(this.$refs.projectModal, {
+                keyboard: false,
+            });
+            modal.show();
+        },
     },
-    {
-        name: 'Bossy Books',
-        company: 'FMU',
-        image: "/src/components/images/projects/bossybooks.png",
-        description: 'Description of Project 3.',
-    },
-];
-
-let selectedProject = null;
-
-const openModal = (index) => {
-    selectedProject = projects[index];
-    console.log(selectedProject)
 };
-
-const closeModal = () => {
-    selectedProject = "Modal Closed";
-};
-
 </script>
 
-<style scoped>
-.project-card {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-color: #eee;
-    padding: 100px;
-    border-radius: 8px;
+<style>
+/* Add your custom styles for project boxes if needed */
+.project-box {
     cursor: pointer;
-    transition: background-color 0.3s;
+    border: 1px solid #ddd;
+    padding: 15px;
+    margin-bottom: 20px;
+    transition: box-shadow 0.3s;
 }
 
-.project-card:hover {
-    background-color: #959494;
-    border-radius: 8px;
-
-}
-
-.project-info {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-
-    width: 100%;
-    padding: 10px;
-    box-sizing: border-box;
-    background-color: rgba(0, 0, 0, 0.7);
-    color: white;
-    transition: opacity 0.3s;
-    opacity: 0;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-
-}
-
-.project-card:hover .project-info {
-    opacity: 1;
-    /* Show text on hover */
+.project-box:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 </style>
