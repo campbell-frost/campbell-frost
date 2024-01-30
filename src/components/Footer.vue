2c0c3a445230853dda26aee6869ad8aab238eab1
campbell-frost <campbellsfrost@gmail.com>
2024-01-30 00:22:39 -0500

    added icons to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,18 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="footer py-2">
-                <p class="pt-1">© Campbell Frost - 2024 | Powered by Vue + Bootstrap</p>
+                <p class="pt-1">
+                    © Campbell Frost - 2024 | Powered by
+                    <img src="./images/vue-icon.png" alt="Vue Icon" class="icon" /> Vue &
+                    <img src="./images/vercel-icon.png" alt="Vercel Icon" class="icon" /> Vercel
+                </p>
             </div>
         </div>
     </div>
 </template>
 
+
 <style scoped>
-    .footer {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        font-size: large;
-    }
+.footer {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: large;
+}
 
-
-
+.icon {
+    max-width: 20px;
+}
 </style>
