197a2b0c4538c0567ef16efa81f391d3bf58266e
campbell-frost <campbellsfrost@gmail.com>
2024-04-04 22:59:29 -0400

    remade about component

--- components/ui/about.tsx
+++ components/ui/about.tsx
@@ -1,26 +1,52 @@
 import { Highlight } from "@/components/ui/hero-highlight";
-import { CardContainer } from "./3d-card";
 import { MeCard } from "./me-card";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+
+import Image from "next/image";
+
 export default function About() {
     return (
-        <div className="container mx-auto px-8">
-            <div className="flex flex-wrap items-center justify-between pt-40">
-                <div className="w-full md:w-1/2 mb-10 md:mb-0">
+        <CardContainer>
+            <CardBody className=" flex items-center justify-between container dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:border-white/[0.2] border-black/[0.1] w-auto  h-auto rounded-xl p-6 border  ">
 
-                    <h5 className="text-white text-4xl font-semibold mb-5 ">Hi, I’m Campbell</h5>
-                    <div className="mb-5">
+                <div className="w-full md:w-1/2 mb-10 md:mb-0">
+                    <CardItem
+                        translateZ="50"
+                        className="text-xl font-bold text-neutral-600 dark:text-white"
+                    >
+                        Hi, I'm Campbell
+                    </CardItem>
+                    <CardItem
+                        as="p"
+                        translateZ="60"
+                        className="text-neutral-500 text-sm  mt-2 dark:text-neutral-300"
+                    >
                         <h1 className="text-white text-6xl font-bold mb-10 leading-tight">
                             I am a full stack <Highlight>software engineer</Highlight> with a passion for crafting digital solutions
                         </h1>
+                    </CardItem>
+                    <CardItem
+                        translateZ="50"
+                        className="text-white text-lg font-semibold"
+                    >
 
-                        <p className="text-white text-lg font-semibold">Contact me - <a href="mailto:campbellsfrost@gmail.com" className="email-link">campbellsfrost@gmail.com</a></p>
-                    </div>
+                        <p >Contact me - <a href="mailto:campbellsfrost@gmail.com" className="email-link">campbellsfrost@gmail.com</a></p>
+                    </CardItem>
                 </div>
-                <div className="w-full md:w-1/3 ">
-                    <MeCard />
+                <div className="w-full md:w-1/3">
+                    <CardItem translateZ="100" className="w-full mt-4">
+                        <Image
+                            src="/images/me_rock.png"
+                            height="1000"
+                            width="1000"
+                            className="w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                            alt="thumbnail"
+                        />
+                    </CardItem>
                 </div>
-            </div>
 
-        </div>
+
+            </CardBody>
+        </CardContainer>
     );
 }
