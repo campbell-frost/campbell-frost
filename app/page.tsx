e054d1d313f7ef48dae4a210d78ed3b5c6053d5b
campbell-frost <campbellsfrost@gmail.com>
2024-04-05 02:18:07 -0400

    in progress technologies

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import About from "@/components/about";
 import Navbar from "@/components/navbar";
+import Spacer from "@/components/spacer";
+import Technologies from "@/components/technologies";
 export default function Home() {
   return (
     <main>
       
         <Navbar />
         <About />
-  
-
+        <Spacer />
+        <Technologies />
     </main>
   );
 }
