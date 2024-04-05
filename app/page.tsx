b602e144a8fa6fc1d515c57ea090702917d891d1
campbell-frost <campbellsfrost@gmail.com>
2024-04-05 07:19:04 -0400

    Added Technologies

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import About from "@/components/about";
 import Navbar from "@/components/navbar";
-import Spacer from "@/components/spacer";
 import Technologies from "@/components/technologies";
+import { HeroGrid } from "@/components/ui/hero-grid";
 export default function Home() {
   return (
     <main>
-      
+      <HeroGrid>
+
         <Navbar />
         <About />
-        <Spacer />
         <Technologies />
+      </HeroGrid>
     </main>
   );
 }
