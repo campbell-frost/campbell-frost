bd78bfa2f689d44d67867199385078ae83feb6bf
campbell-frost <campbellsfrost@gmail.com>
2024-04-05 00:46:36 -0400

    added margin around about content

--- components/ui/about.tsx
+++ components/ui/about.tsx
@@ -1,13 +1,13 @@
 import { Highlight } from "@/components/ui/hero-highlight";
-import { MeCard } from "./me-card";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 
 import Image from "next/image";
 
 export default function About() {
     return (
-        <CardContainer>
-            <CardBody className=" flex flex-wrap items-center justify-between container dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:border-white/[0.2] border-black/[0.1] w-auto  h-auto rounded-xl p-6 border  ">
+        <CardContainer className="container mx-auto">
+            
+            <CardBody className="flex flex-wrap items-center justify-between dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:border-white/[0.2] border-black/[0.1] w-auto  h-auto rounded-xl p-6 border">
 
                 <div className="w-full md:w-1/2 mb-10 md:mb-0">
                     <CardItem
