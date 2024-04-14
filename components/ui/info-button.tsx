35d10fb8717ba3d43a146eafa29422a7f70a8b1a
campbell-frost <campbellsfrost@gmail.com>
2024-04-13 22:26:51 -0400

    fixed button padding

--- components/ui/info-button.tsx
+++ components/ui/info-button.tsx
@@ -8,16 +8,16 @@ interface InfoButtonProps {
 export default function InfoButton({ name, link }: InfoButtonProps) {
     return (
         <button className="p-[3px] relative mx-4 border-b-2 border-transparent hover:border-white transition-border duration-300">
+            <a
+                href={link}
+                role="button"
+                aria-pressed="true"
+            >
             <div className="absolute inset-0 " />
-            <div className="px-4 py-2 relative group text-white ">
-                <a
-                    href={link}
-                    role="button"
-                    aria-pressed="true"
-                >
+                <div className="px-4 py-2 relative group text-white ">
                     {name}
-                </a>
-            </div>
+                </div>
+            </a>
         </button>
     );
 }
