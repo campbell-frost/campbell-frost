e9e278d874dccb4162f8db903eefda43be69d1bc
campbell-frost <campbellsfrost@gmail.com>
2024-08-03 21:26:07 -0400

    reverted technologies section

--- components/technologies.tsx
+++ components/technologies.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-
-import { InfiniteMovingCards } from "./ui/moving-cards";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import { Icon } from "./ui/icon";
 
 const TechComponent = () => {
-  const techList1 = [
+  const techList = [
     {
       image:
         "https://devblogs.microsoft.com/aspnet/wp-content/uploads/sites/16/2019/04/BrandBlazor_nohalo_1000x.png",
@@ -14,10 +14,6 @@ const TechComponent = () => {
       image:
         "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
       text: "React.js",
-    },
-    {
-      image: "./images/Go-Logo_Blue.png",
-      text: "Go",
     },
     {
       image: "https://www.svgrepo.com/show/354528/vue.svg",
@@ -31,9 +27,6 @@ const TechComponent = () => {
       image: "./images/csharp.png",
       text: "C#",
     },
-  ];
-
-  const techList2 = [
     {
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/1024px-Microsoft_.NET_logo.svg.png",
@@ -43,10 +36,6 @@ const TechComponent = () => {
       image:
         "https://www.wpgraphql.com/_next/image?url=%2Flogos%2Flogo-nextjs.png&w=384&q=75",
       text: "Next.js",
-    },
-    {
-      image: "./images/Typescript_logo_2020.svg.png",
-      text: "TypeScript",
     },
     {
       image:
@@ -68,8 +57,31 @@ const TechComponent = () => {
   return (
     <div className=" py-20">
       <h1 className=" font-bold text-3xl mb-10">Technologies I Use</h1>
-      <InfiniteMovingCards items={techList1} direction="left" speed="normal" />
-      <InfiniteMovingCards items={techList2} direction="right" speed="normal" />
+      <div className="grid grid-cols-2 text-white md:grid-cols-5 gap-16 max-h-90 max-w-90">
+        {techList.map((tech, index) => (
+          <CardContainer divisiorValue={1000}>
+            <CardBody>
+              <CardItem translateZ={50}>
+                <div
+                  key={index}
+                  className="border bg-black border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative"
+                >
+                  <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+                  <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+                  <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+                  <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+                  <img
+                    className="tech-icon object-contain rounded"
+                    src={tech.image}
+                    alt={tech.text}
+                  />
+                  <div className="absolute opacity-0 rounded-lg"></div>
+                </div>
+              </CardItem>
+            </CardBody>
+          </CardContainer>
+        ))}
+      </div>
     </div>
   );
 };
